fcfee4f86c22183c335b74302e39470d6d7319be
Viktar Belski <Viktar_Belski@epam.com>
2020-02-26 14:12:33 +0300

    feat: update angular & adding firebase

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Oshop
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.5 and updated to version 9.0.
 
 ## Development server
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBO8e9k11-lRSRSCx6K1cDGdvp8W_Fq8zQ',
+    authDomain: 'oshop-b1cbf.firebaseapp.com',
+    databaseURL: 'https://oshop-b1cbf.firebaseio.com',
+    projectId: 'oshop-b1cbf',
+    storageBucket: 'oshop-b1cbf.appspot.com',
+    messagingSenderId: '225503699159',
+    appId: '1:225503699159:web:32cc4e1c7f674ffbe8d092',
+    measurementId: 'G-ZZNVY2645S'
+  }
 };
 
 /*
