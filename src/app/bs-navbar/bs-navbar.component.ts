e72886024b4ca8222d933baf70cf08b2459f2fdc
Viktar Belski <belski.viktor.dev@gmail.com>
2020-03-01 01:26:49 +0300

    feat: firebase user info

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-bs-navbar',
@@ -7,8 +8,10 @@ import { AngularFireAuth } from 'angularfire2/auth';
   styleUrls: ['./bs-navbar.component.scss']
 })
 export class BsNavbarComponent {
+  user: firebase.User;
 
   constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
   logout() {
