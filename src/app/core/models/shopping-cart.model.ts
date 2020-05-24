e97751bb83fbbf421a000a223f0a81b35136e705
Viktar Belski <belski.viktor.dev@gmail.com>
2020-05-25 00:25:18 +0300

    feat: shopping cart component

--- src/app/core/models/shopping-cart.model.ts
+++ src/app/core/models/shopping-cart.model.ts
@@ -1,12 +1,15 @@
 import { ShoppingCartItem } from './shopping-cart-item.model';
 
 export class ShoppingCart {
+  items: ShoppingCartItem[];
 
   constructor(items: ShoppingCartItem[]) {
     this.items = items;
   }
 
-  items: ShoppingCartItem[];
+  get productIds() {
+    return Object.keys(this.items);
+  }
 
   get totalQuantity(): string {
     let count = 0;
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -10,6 +10,7 @@ import { ShoppingCart } from '../core/models/shopping-cart.model';
 })
 export class ShoppingCartComponent implements OnInit {
   cart$: Observable<ShoppingCart>;
+  displayedColumns: string[] = ['title', 'quantity'];
 
   constructor(private shoppingCardService: ShoppingCartService) {
   }
