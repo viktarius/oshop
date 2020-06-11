c067864be0d54a5a91a54629c496caa60cb529f0
Viktar Belski <belski.viktor.dev@gmail.com>
2020-06-11 19:59:24 +0300

    feat: shopping-cart module

---
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+
+import { ShoppingCartComponent } from './shopping-cart.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ShoppingCartComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatTableModule,
+  ],
+  exports: [
+    ShoppingCartComponent]
+})
+export class ShoppingCartModule { }
