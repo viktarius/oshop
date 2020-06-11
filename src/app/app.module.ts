6c9840df59945db3a9125eb450e77cb9b01d3220
Viktar Belski <belski.viktor.dev@gmail.com>
2020-06-11 20:21:15 +0300

    feat: my-orders module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { MatTableModule } from '@angular/material/table';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
-import { MatListModule } from '@angular/material/list';
-import { MatBadgeModule } from '@angular/material/badge';
+
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AdminModule } from './admin/admin.module';
 import { ProductsModule } from './products/products.module';
@@ -32,11 +18,11 @@ import { LoginModule } from './login/login.module';
 import { CheckOutModule } from './check-out/check-out.module';
 import { CoreModule } from './core/core.module';
 import { OrderSuccessModule } from './order-success/order-success.module';
+import { MyOrdersModule } from './my-orders/my-orders.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyOrdersComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,21 +40,7 @@ import { OrderSuccessModule } from './order-success/order-success.module';
     ShoppingCartModule,
     CheckOutModule,
     OrderSuccessModule,
-
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatInputModule,
-    MatGridListModule,
-    MatCardModule,
-    MatSelectModule,
-    MatListModule,
-    MatBadgeModule
+    MyOrdersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
